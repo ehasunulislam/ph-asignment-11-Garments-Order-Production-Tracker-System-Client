f8a46030d57ed4d52519b134e2b5f621f1995dbb
ehasun15019 <ehasunulislam0202@gmail.com>
2025-12-08 12:30:59 +0600

    Change the page loading animation

--- src/Components/Loading/PageLoading.jsx
+++ src/Components/Loading/PageLoading.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { assets } from "../../assets/assets";
-import Lottie from "lottie-react";
+// import Lottie from "lottie-react";
+import "./PageLoading.css"
 
 const PageLoading = () => {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
-      <Lottie
+      {/* <Lottie
         animationData={assets.pageLoading}
         loop={true}
         className="w-32 h-32 md:w-58 md:h-58"
-      />
+      /> */}
+
+      <div>
+        <img src={assets.logo} alt="logo" />
+        <h3 className="text-2xl font-bold typing-animation">CartZilla</h3>
+      </div>
     </div>
   );
 };
