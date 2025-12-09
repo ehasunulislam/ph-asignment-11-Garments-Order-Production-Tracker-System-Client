38bcd18874819f477ae1218f5eb2360e003e4faa
ehasun15019 <ehasunulislam0202@gmail.com>
2025-12-09 23:54:28 +0600

    update AdminProfile for user's role

--- src/Page/Dashboard/AdminProfile.jsx
+++ src/Page/Dashboard/AdminProfile.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import useAuthInfo from "../../Components/Hooks/useAuthInfo";
+import useAxios from "../../Components/Hooks/useAxios";
+import { useQuery } from "@tanstack/react-query";
+import PageLoading from "../../Components/Loading/PageLoading";
+import Swal from "sweetalert2";
 
 const AdminProfile = () => {
-  const { user } = useAuthInfo();
+  const { user, signOutFunction } = useAuthInfo();
+  const axiosInstance = useAxios();
+
+  const { data: userData = {}, isLoading, error } = useQuery({
+    queryKey: ["userData", user?.email],
+    enabled: !!user?.email,
+    queryFn: async () => {
+      const res = await axiosInstance.get(`/users/${user.email}`);
+      return res.data;
+    },
+  });
+
+
+  // handle signout functionality 
+  const handleSignOut = () => {
+    signOutFunction()
+      .then(() => {
+        Swal.fire({
+          title: "Drag me!",
+          icon: "success",
+          draggable: true,
+        });
+      })
+      .catch((error) => {
+        Swal.fire({
+          icon: "error",
+          title: error.message,
+          text: "Something went wrong!",
+          footer: '<a href="#">Why do I have this issue?</a>',
+        });
+      });
+  };
+  
+
+
+  if(isLoading) {
+    return <PageLoading />
+  }
+
+  if(error) {
+    return <p>Have some error</p>
+  }
 
   return (
     <div className="flex justify-center items-center">
@@ -16,8 +61,9 @@ const AdminProfile = () => {
         </figure>
         <div className="card-body items-center text-center">
           <h2 className="card-title">{user.displayName}</h2>
+          <p>Role: {userData?.role || ""}</p>
           <div className="card-actions">
-            <button className="btn btn-primary">Edit Profile</button>
+            <button className="btn btn-primary" onClick={handleSignOut}>Log out</button>
           </div>
         </div>
       </div>
